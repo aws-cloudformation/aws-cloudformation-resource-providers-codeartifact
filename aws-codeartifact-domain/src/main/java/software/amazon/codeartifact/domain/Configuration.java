409fd0677eb69f43d6223a3b527f097d768a7201
Jon Miguel Jara <jonjar@amazon.com>
2020-07-14 19:59:21 +0000

    Init resource handlers

---
+++ aws-codeartifact-domain/src/main/java/software/amazon/codeartifact/domain/Configuration.java
@@ -0,0 +1,8 @@
+package software.amazon.codeartifact.domain;
+
+class Configuration extends BaseConfiguration {
+
+    public Configuration() {
+        super("aws-codeartifact-domain.json");
+    }
+}
